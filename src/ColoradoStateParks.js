16680ac52ceffbf4c98d809a3f3c26a430db0a9a
Iam maria theresa <iam.mariatheresaelkazzi@gmail.com>
2022-09-15 16:15:05 +0300

    done

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,29 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import * as RMFunctions from "./parks/RockyMountain";
+import MesaVerde from "./parks/MesaVerde";
+//import { trees, wildlife } from "./parks/RockyMountain";
+// import {
+//   trees as parkTrees,
+//   wildlife as parkWildlife,
+// } from "./parks/RockyMountain";
+
+
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
+  console.log(parkTrees);
+  //console.log(trees);
+  //console.log(RMFunctions.trees);
+  //RMFunctions.wildlife();
+  //RMFunctions.elevation();
+  //howManyParks(); // => "42 parks!"
+  //wildlife();
+  //parkWildlife();
+  return (
+    <div>
+      <MesaVerdePark />
+    </div>
+  );
 }
+export default ColoradoStateParks;
